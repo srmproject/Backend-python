6d589c8df95a10a5655f24258327fee163325b03
choisungwook <kgg1959@naver.com>
2022-05-07 02:36:39 +0900

    스키마 시간칼럼 추가

--- app/domain/project/models.py
+++ app/domain/project/models.py
@@ -1,12 +1,13 @@
 from database import Base
 from sqlalchemy import String, Integer, Column
+from domain.db_mixins import TimeStamp
 
 
-class Project(Base):
+class Project(TimeStamp, Base):
     __tablename__ = "project"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=True, unique=True)
+    name = Column(String(255), nullable=False, unique=True)
     description = Column(String(255))
 
     def __repr__(self) -> str:
