dd2111496c457601fdf4a470fec8077a7ba5f319
choisungwook <kgg1959@naver.com>
2022-06-08 21:08:32 +0900

    cicd 배포

--- app/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from config import cnf
-from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://{cnf.POSTGRESQL_USER}:{cnf.POSTGRESQL_PASSWORD}@{cnf.POSTGRESQL_HOST}/{cnf.POSTGRESQL_DB}"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
-
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
--- app/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-import yaml
-
-with open('log.yaml', 'r') as f:
-    config = yaml.safe_load(f)
-
-logging.config.dictConfig(config)
-log = logging.getLogger('simple')
