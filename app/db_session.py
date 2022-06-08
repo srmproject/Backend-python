9b3d4139ba6a3a68b0bdc0f4af4026f3cdf1af14
choisungwook <kgg1959@naver.com>
2022-06-08 22:04:34 +0900

    디버깅 로그 추가

--- app/db_session.py
+++ app/db_session.py
@@ -1,10 +1,13 @@
 from database import SessionLocal
+from logger import log
 
 
 def get_db():
     '''DB 세션관리'''
+    log.info("[-] db 세션생성")
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+        log.info("[-] db 세션 종료")
