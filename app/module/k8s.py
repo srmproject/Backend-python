7784e8a9b48068abbda830ab4ffce97a9a709495
choisungwook <kgg1959@naver.com>
2022-06-26 22:10:13 +0900

    k8s 모듈 job생성 추가 #16

--- app/module/k8s.py
+++ app/module/k8s.py
@@ -13,7 +13,7 @@ class JCPK8S:
             log.info("[*][*] dev 설정 로드")
             config.load_incluster_config()
         self.v1 = client.CoreV1Api()
-        
+
     def createNamespace(self, namespace) -> None:
         """
         namespace 생성
@@ -35,3 +35,55 @@ class JCPK8S:
         """
         ns = client.V1Namespace()
         self.v1.delete_namespace(namespace)
+
+    def generateContainerTemplate(self, name, image, args=None, command=None) -> client.V1Container:
+        """컨테이너 템플릿 생성"""
+        return client.V1Container(
+            name=name,
+            image=image,
+            image_pull_policy="Always",
+            args=args,
+            command=command
+        )
+
+    def generatePodTemplate(self, name: str, namespace: str, containers) -> client.V1PodTemplateSpec:
+        """
+        pod 템플릿 생성
+        :param name:
+        :param namespace:
+        :return:
+        """
+        return client.V1PodTemplateSpec(
+            spec=client.V1PodSpec(restart_policy="Never", containers=[containers]),
+            metadata=client.V1ObjectMeta(name=name, labels={"app": name}),
+        )
+
+    def createJobTemplate(self, namespace: str, job_name: str, pod_template: client.V1PodTemplateSpec):
+        """
+        job 생성
+
+        :param namespace: namespace 이름
+        :param job_name: job 이름
+        :return:
+        """
+
+        metadata = client.V1ObjectMeta(
+            name=job_name,
+            namespace=namespace,
+            labels={"app": job_name}
+        )
+        spec = client.V1JobSpec(
+            backoff_limit=0,
+            template=pod_template
+        )
+
+        return client.V1Job(
+            api_version="batch/v1",
+            kind="Job",
+            metadata=metadata,
+            spec=spec
+        )
+
+    def createJob(self, namespace, job_template) -> client.V1Job:
+        batch_api = client.BatchV1Api()
+        return batch_api.create_namespaced_job(namespace, job_template)
---
+++ app/tests/test_k8s.py
@@ -0,0 +1,31 @@
+from module import k8s
+from kubernetes import client, config
+
+# def test_create_namespace():
+#     jcp_k8s = k8s.JCPK8S()
+#
+#     namespace = "jcp-k8s-test1"
+#     jcp_k8s.createNamespace(namespace=namespace)
+
+
+def test_create_job():
+    """job 생성"""
+    jcp_k8s = k8s.JCPK8S()
+
+    namespace = "default"
+    job_name = "jcp-k8s-jobtest-1"
+
+    container_template = jcp_k8s.generateContainerTemplate(name=job_name, image="busybox")
+    pod_template = jcp_k8s.generatePodTemplate(
+        name=job_name,
+        namespace=namespace,
+        containers=container_template,
+    )
+
+    job_template = jcp_k8s.createJobTemplate(
+        namespace=namespace,
+        job_name=job_name,
+        pod_template=pod_template
+    )
+
+    created_job_obj = jcp_k8s.createJob(namespace=namespace, job_template=job_template)
