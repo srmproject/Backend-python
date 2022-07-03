650776a8a006ca818178632e4273d7dc937a254e
choisungwook <kgg1959@naver.com>
2022-07-03 17:08:13 +0900

    세션 생성 로그 삭제

--- app/db_session.py
+++ app/db_session.py
@@ -4,10 +4,8 @@ from logger import log
 
 def get_db():
     '''DB 세션관리'''
-    log.info("[-] db 세션생성")
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-        log.info("[-] db 세션 종료")
